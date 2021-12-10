15ba7e8d2e907badc79816d003bbbbedb78879fe
RanaNitishSingh <nitish.zeroit@gmail.com>
2021-12-10 10:38:22 +0530

    Add Class and viewController

---
+++ FaceID TouchID/DemoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DEMO\ViewController.swift
+//  FaceID TouchID
+//
+//  Created by Imac on 10/12/21.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+    
+    
+    var see : String = "qwertyuiopasdfgjklzxcvbnmqwertyuiopasdfghjklzxcvbnm"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
